0b85993ce52bfae689048bcdf60659a2205e6c95
ekarahodza1 <56482990+ekarahodza1@users.noreply.github.com>
2023-11-21 22:42:43 +0100

    code refactoring done

--- Disarm-Add-In/src/taskpane/taskpane.js
+++ Disarm-Add-In/src/taskpane/taskpane.js
@@ -2,115 +2,133 @@
  * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
  * See LICENSE in the project root for license information.
  */
-
 /* global     document, Office, Word */
+
+
 const fs = require('fs');
 'use strict';
 
-const pluginDialog = document.getElementById('plugin-dialog');
-const optionSelect = document.getElementById('option-select');
-const applyBtn = document.getElementById('apply-btn');
-const redTags = []
+var jsonTechniques = '';
+var reverseJsonTechniques = '';
 
 var redTagColorGlobal = "#FFFF00";
 
 Office.onReady((info) => {
   if (info.host === Office.HostType.Word) {
+
+    //red tag
     document.getElementById("insert-red-tag").onclick = () => tryCatch(insertRedTag);
     document.getElementById("search-red-tag").onclick = () => tryCatch(searchRedTag);
+
+    //summaries
     document.getElementById("insert-red-table").onclick = () => tryCatch(insertRedTable);
+
+    //formatting
     document.getElementById("format-red-tag-color").onclick = () => tryCatch(changeRedTagColor);
+
+    //save
     document.getElementById("save-btn").onclick = () => tryCatch(saveButton);
     document.getElementById("save-btn1").onclick = () => tryCatch(displaySearchTechniques);
     document.getElementById("save-btn2").onclick = () => tryCatch(changeRedTagColorSaveBtn);
-    document.getElementById("close-btn").onclick = () => tryCatch(closeButton);
-    document.getElementById("close-btn1").onclick = () => tryCatch(closeButton);
-    document.getElementById("close-btn2").onclick = () => tryCatch(closeButton);
+
+    //close
+    document.querySelectorAll('.close').forEach(element => element.onclick = () => tryCatch(closeButton));
+
   }
 });
 
+
 async function tryCatch(callback) {
   try {
-      await callback();
+    await callback();
   } catch (error) {
-      // Note: In a production add-in, you'd want to notify the user through your add-in's UI.
-      console.error(error);
+    console.error(error);
   }
 }
+
+async function closeButton(){
+  await Word.run(async (context) => {
+    const popup1 = document.getElementById('popup-insert-red-tag');
+    popup1.style.display = 'none';
+
+    const popup2 = document.getElementById('popup-red-tag-formatting');
+    popup2.style.display = 'none';
+
+    const popup3 = document.getElementById('popup-search-techniques');
+    popup3.style.display = 'none';
+
+    const popup4 = document.getElementById('popup-search-techniques-list');
+    popup4.style.display = 'none';
+
+  })
+}
+
+
+//red tag 
 
 async function insertRedTag() {
   await Word.run(async (context) => {
 
-    const pluginContainer = document.getElementById('plugin-container');
+    //initialization of used components
+    const popup = document.getElementById('popup-insert-red-tag');
     const option1 = document.getElementById('option1');
     const option2 = document.getElementById('option2');
     const option3 = document.getElementById('option3');
     const saveBtn = document.getElementById('save-btn');
-
 
     option1.value = '';
     option2.value = '';
     option2.innerHTML = '';
     option3.style.display = 'none';
 
-    pluginContainer.style.display = 'block';
+    popup.style.display = 'block';
 
-    $.ajax({
-      dataType: "json",
-      url: "https://api.jsonbin.io/v3/b/651a869054105e766fbc92de",
-      data: {
-        'X-Master-Key': '111111$2a$10$aGFnKNoSPkQ7mmpPofdEqe9aZSNnEVBqKCXEkuwyl5OWz8kDfPbw',
-      },
-      success: function (result, status, xhr) {
-    
-          dataObject = result.record
-          dataObject1 = dataObject.plan
-          planOption = dataObject.plan
-          chosenOption = planOption
-          
-          
-      },
-      error: function (xhr, status, error) {
-              console.log(error) 
-      }})
+    //getting data from the server only once
+    if (jsonTechniques == '') await getTechniques();
 
+    planOption = jsonTechniques.plan
+    chosenOption = planOption
+
+    //listening for a change in the first dropdown
     option1.addEventListener('change', function() {
       selectedOption = option1.value;
 
       if (selectedOption === 'option1') {
-        planOption = dataObject.plan
+        planOption = jsonTechniques.plan
         chosenOption = planOption
         jsonArray = Object.keys(planOption)
           
       } else if (selectedOption === 'option2') {
-        prepareOption = dataObject.prepare
+        prepareOption = jsonTechniques.prepare
         chosenOption = prepareOption
         jsonArray = Object.keys(prepareOption)
 
       } else if (selectedOption === 'option3'){
-        executeOption = dataObject.execute
+        executeOption = jsonTechniques.execute
         chosenOption = executeOption
         jsonArray = Object.keys(executeOption)
 
-
       } else if (selectedOption === 'option4'){
-        assessOption = dataObject.assess
+        assessOption = jsonTechniques.assess
         chosenOption = assessOption
         jsonArray = Object.keys(assessOption)
+
       } else { 
           option2.innerHTML = '';
       }
 
-      // Show the second dropdown
+      // showing the change in the second dropdown
       option2.innerHTML = ''
+
       jsonArray.forEach(item => { option2.innerHTML += '<option value=\"' + item +"\">" + item + "</option>"});
         
       option2.style.display = 'block';
       option2.value = '';
-  });
+    });
 
-  option2.addEventListener('change', function() {
-      // Show the third subcomponent
+    option2.addEventListener('change', function() {
+      // showing the techniques in a list of checkboxes
+
       selectedOption.option2 = option2.value;
       option3.style.display = 'block';
 
@@ -119,57 +137,118 @@ async function insertRedTag() {
       option3.innerHTML = ''
       array.forEach(item => { option3.innerHTML += '<label><input type=\"checkbox\" value=\"' + Object.keys(item)[0] +"\">" + Object.values(item)[0] + "</label><br>"})
  
-
-  });
-
+    });
   });
 }
+
+async function saveButton(){
+  await Word.run(async (context) => {
+
+    // get values from checkboxes
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    const popup = document.getElementById('popup-insert-red-tag');
+
+    let text = '('
+
+    checkboxes.forEach(checkbox => {
+      if (checkbox.checked) {
+          text += checkbox.nextSibling.textContent.trim() + " [" + checkbox.value + "], "
+        }
+    });
+
+    
+    text = text.slice(0, -2); 
+
+    text += ')'
+
+    popup.style.display = 'none';
+    option1.value = '';
+    option2.value = '';
+    option3.style.display = 'none';
+
+    var doc = context.document.getSelection();
+    context.load(doc)
+    var selectedLength = await context.sync().then(() => {return doc.text.length })
+
+    // if nothing is selected get the entire sentence
+    if (selectedLength < 1){
+      var doc1 = context.document.getSelection().getTextRanges(['\n', '.', '?'], false);
+      context.load(doc1);
+      doc =  await context.sync().then(() => { return doc1.items[0]})
+    }
+
+    //highlght the text
+    doc.font.set({highlightColor: redTagColorGlobal});
+    
+    //insert text with tags
+    doc.insertText(text, Word.InsertLocation.end);
+    text = ''
+
+    await context.sync(); 
+  })
+}
+
+async function getTechniques(){
+  $.ajax({
+    dataType: "json",
+    url: "https://api.jsonbin.io/v3/b/651a869054105e766fbc92de",
+    data: {
+      'X-Master-Key': '111111$2a$10$aGFnKNoSPkQ7mmpPofdEqe9aZSNnEVBqKCXEkuwyl5OWz8kDfPbw',
+    },
+    success: function (result, status, xhr) {
+      jsonTechniques = result.record
+    },
+    error: function (xhr, status, error) {
+      console.log(error) 
+  }})
+}
+
+async function searchRedTag(){
+  const popup = document.getElementById('popup-search-techniques');
+  popup.style.display = 'block';
+}
+
+async function displaySearchTechniques(){
+  const popup1 = document.getElementById('popup-search-techniques');
+  popup1.style.display = 'none';
+
+  const popup2 = document.getElementById('popup-search-techniques-list');
+  popup2.style.display = 'block';
+}
+
+
+//summaries
 
 async function insertRedTable() {
 
   await Word.run(async (context) => {
-
-    const doc = context.document
     
-    $.ajax({
-      dataType: "json",
-      url: "https://api.jsonbin.io/v3/b/6527deaf12a5d376598ac60e",
-      headers: {'X-Master-Key': '$2a$10$aGFnKNoSPkQ7mmpPofdEqe9aZSNnEVBqKCXEkuwyl5OWz8kDfPbw.' },
-      data: {
-        'X-Master-Key': '$2a$10$aGFnKNoSPkQ7mmpPofdEqe9aZSNnEVBqKCXEkuwyl5OWz8kDfPbw.',
-      },
-      success: async function (result, status, xhr) {
-    
-          dataObject = result.record
-
-      const tag_ids = dataObject.ids
-      
+    //get search json only once
+    if (reverseJsonTechniques == '') await searchTechniques();
     
 
-    var doc1 = context.document.body
-    doc1.load("text")
+    const tag_ids = reverseJsonTechniques.ids
+    
+    // get text of the entire document
+    var wholeDocument = context.document.body
+    wholeDocument.load("text")
     await context.sync();
+    var text = wholeDocument.text
 
-    var text = doc1.text
-
+    //match cases using regular expressions
     const smallBracketRegex = /\(([^()]*)\)/g;
-    //const foundSmallBrackets1 = text.match(smallBracketRegex);
-
-   const foundSmallBrackets = text.matchAll(/\(([^()]*)\)/g);
-
-
     const middleBracketRegex = /\[(.*?)\]/g;
+
+    const foundSmallBrackets = text.matchAll(smallBracketRegex);
+
     let foundMiddleBrackets = []
-
     let table = [["Tecnique title", "ID", "Text", "Use"]]
-
-    
 
     for (const matches of foundSmallBrackets){
   
       foundMiddleBrackets = matches[0].match(middleBracketRegex);
       const commasNumber = (matches[0].match(/,/g) || []).length;
-      let selectedText = extractText(text, matches.index)
+      let selectedText = extractTextFromBrackets(text, matches.index)
 
       if (foundMiddleBrackets.length != commasNumber + 1 || foundMiddleBrackets.length == 0) continue;
 
@@ -177,31 +256,13 @@ async function insertRedTable() {
         
         let tag = foundMiddleBrackets[j].slice(1, -1)
         let tagObject = tag_ids[tag]
-
         
-        table.push([tagObject.title, tag, selectedText, tagObject.use])
-        
+        // add matched cases into table
+        table.push([tagObject.title, tag, selectedText, tagObject.use]) 
       }
-
-
     }
-
     drawTable(table)
-
-
-          
-  },
-  error: function (xhr, status, error) {
-          console.log(error) 
-  }})
-
-
- 
   }
-
-
-    
-  
 )}
 
 async function drawTable(table){
@@ -214,84 +275,7 @@ async function drawTable(table){
 
 };
 
-async function saveButton(){
-  await Word.run(async (context) => {
-
-  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-  const pluginContainer = document.getElementById('plugin-container');
-
-      let text = '('
-
-      checkboxes.forEach(checkbox => {
-        if (checkbox.checked) {
-            text += checkbox.nextSibling.textContent.trim() + " [" + checkbox.value + "], "
-        }
-
-      
-    });
-
-    
-    text = text.slice(0, -2); 
-
-    text += ')'
-
-    pluginContainer.style.display = 'none';
-
-      // Clear selections and reset visibility
-    option1.value = '';
-    option2.value = '';
-    option3.style.display = 'none';
- 
-
-    
- 
-
-  var doc = context.document.getSelection();
-  context.load(doc)
-  var selectedLenth = await context.sync().then(() => {return doc.text.length })
-
-
-  if (selectedLenth < 1){
-   var doc1 = context.document.getSelection().getTextRanges(['\n', '.', '?'], false);
-   context.load(doc1);
-    //var sentence = await context.sync().then(() => {return doc1.items[0].text;})
-  
-   doc =  await context.sync().then(() => { return doc1.items[0]})
-   
-
-  
-  }
-
-  doc.font.set({highlightColor: redTagColorGlobal});
-    
-
-   doc.insertText(text, Word.InsertLocation.end);
-    text = ''
-
-
-
-    await context.sync(); 
-  })
-
-}
-
-async function closeButton(){
-  await Word.run(async (context) => {
-    const pluginContainer = document.getElementById('plugin-container');
-    pluginContainer.style.display = 'none';
-
-    const pluginContainer1 = document.getElementById('plugin-container-red-tag');
-    pluginContainer1.style.display = 'none';
-
-    const pluginContainer2 = document.getElementById('plugin-container-search-techniques');
-    pluginContainer2.style.display = 'none';
-
-
-    
-  })
-}
-
-function extractText(str, endIndex) {
+function extractTextFromBrackets(str, endIndex) {
   const pattern = /[.!?]/;
   let s = str.substring(0, endIndex - 2)
   let index = s.lastIndexOf(pattern.exec(s)) + 1
@@ -299,32 +283,42 @@ function extractText(str, endIndex) {
   return str.substring(index, endIndex);
 }
 
+async function searchTechniques(){
+  
+  $.ajax({
+    dataType: "json",
+    url: "https://api.jsonbin.io/v3/b/655cf2010574da7622c9d82b",
+    data: {
+      'X-Master-Key': '111111$2a$10$aGFnKNoSPkQ7mmpPofdEqe9aZSNnEVBqKCXEkuwyl5OWz8kDfPbw',
+    },
+    success: function (result, status, xhr) {
+      var resp = result.record
+      reverseJsonTechniques =resp
+    },
+    error: function (xhr, status, error) {
+      console.log(error) 
+  }})
+}
+
+
+//formatting
+
 function changeRedTagColor(){
-  const pluginContainer = document.getElementById('plugin-container-red-tag');
+  const popup = document.getElementById('popup-red-tag-formatting');
   const colorPicker = document.getElementById('colorPicker');
   colorPicker.value = redTagColorGlobal;
 
-  pluginContainer.style.display = 'block';
+  popup.style.display = 'block';
 
 }
 
 async function changeRedTagColorSaveBtn(){
-  const pluginContainer = document.getElementById('plugin-container-red-tag');
+  const popup = document.getElementById('popup-red-tag-formatting');
   var selectedColor = document.getElementById('colorPicker').value;
   redTagColorGlobal = selectedColor;
-  pluginContainer.style.display = 'none';
+  popup.style.display = 'none';
 }
 
-async function searchRedTag(){
-  const pluginContainer = document.getElementById('plugin-container-search-techniques');
-  pluginContainer.style.display = 'block';
-}
 
-async function displaySearchTechniques(){
-  const pluginContainer = document.getElementById('plugin-container-search-techniques');
-  pluginContainer.style.display = 'none';
 
-  const pluginContainer1 = document.getElementById('plugin-container-search-techniques-list');
-  pluginContainer1.style.display = 'block';
-}
 
