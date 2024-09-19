982009121326365f4fcac3187089d16a11f0e636
ekarahodza1 <56482990+ekarahodza1@users.noreply.github.com>
2024-09-19 17:11:31 +0200

    refactoring on search and inert table

--- Disarm-Add-In/src/components/red-tag-component.js
+++ Disarm-Add-In/src/components/red-tag-component.js
@@ -13,6 +13,21 @@ var jsonTechniques = '';
 var reverseJsonTechniques = '';
 var searchTechniquesArray = []
 
+$(document).on('click','tr',function(e) { handleTableColorOnClick.call(this) }); 
+$(document).on('click', 'th', function(e) {
+  e.stopPropagation(); 
+  sortTechniquesTable.call(this);
+});
+
+export async function insertRedTag() {
+  await Word.run(async (context) => {
+    helpersUI.blurBackground();
+    await showPhaseOptions(true);
+    helpersUI.clearInsertRedTagFields();
+  });
+}
+
+
 export async function showPhaseOptions(insertTag){
     jsonTechniques = await getTechniques();
     const uiField = insertTag ? phaseChooseField : phaseSearchField;
@@ -55,7 +70,7 @@ function showTacticOptions(phase, insertTag){
     })
 }
   
-export async function saveButton(){
+export async function saveButtonInsertTechniques(){
     await Word.run(async (context) => {
   
       const checkboxes = document.querySelectorAll('input[type="checkbox"]');
@@ -85,4 +100,123 @@ export async function saveButton(){
       helpersUI.unblurBackground();
       await context.sync(); 
     })
+}
+
+export async function searchRedTag(){
+  
+  searchTechniquesPopup.style.display = 'block';
+  tacticSearchField.innerHTML = '';
+  helpersUI.blurBackground()
+  await showPhaseOptions()  
+}
+
+export async function displaySearchTechniques(){
+  if (textBox.value == "") {
+    alertSearchBox.innerHTML = 'Please fill out this field!';
+  }
+  else {
+    searchTechniquesPopup.style.display = 'none';
+
+    if (reverseJsonTechniques == "") reverseJsonTechniques = await searchTechniques()
+
+    searchTechniquesArray = helpers.searchTechniquesFromJson(
+      helpers.getSelectedRowText(phaseSearchField), 
+      helpers.getSelectedRowText(tacticSearchField), 
+      checkbox.checked, 
+      textBox.value, 
+      reverseJsonTechniques)
+
+    helpersUI.drawFoundTechniquesTable(searchTechniquesArray)
+
+    listSearchTechniquesPopup.style.display = 'block';
+    textBox.value = '';
+    checkbox.checked = ''
+    alertSearchBox.innerHTML = ''
+  }
+}
+
+export async function saveButtonSearchTechniques(context){
+  await Word.run(async (context) => {
+  const tagText = helpers.createTagTextSearch(table)
+
+    listSearchTechniquesPopup.style.display = 'none';
+    phaseSearchField.innerHTML = '';
+    tacticSearchField.innerHTML = '';
+    helpersUI.unblurBackground();
+
+    //get sentence
+    var sentenceInitialRange = context.document.getSelection().getTextRanges(['\n', '.', '?'], true);
+    context.load(sentenceInitialRange);
+    var sentenceRange =  await context.sync().then(() => { return sentenceInitialRange.items[0]})
+
+    var sentenceWithoutDot = (sentenceRange.text).slice(0, -1); 
+     
+    //insert tag text to end of sentence
+    sentenceRange.insertText(sentenceWithoutDot + ' ' + tagText + '.', Word.InsertLocation.replace);
+
+    //search tag inside range to highlight
+    var rangeToHighlight = sentenceRange.search(tagText);
+    rangeToHighlight.load('items');
+    await context.sync();
+   
+    //change highlight color of the tag
+    rangeToHighlight.items[0].font.set({highlightColor: getRedTagColor()});
+
+    searchTechniquesArray = []
+
+    await context.sync(); 
+  })
+}
+
+export function clearSearchTechniquesArray(){
+  searchTechniquesArray = [];
+}
+
+function sortTechniquesTable(){
+
+  var column = $(this).text().trim();
+  switch (column) {
+    case "Phase":
+      searchTechniquesArray.sort((a, b) => a.phase.localeCompare(b.phase));
+      break;
+    case "Tactic":
+      searchTechniquesArray.sort((a, b) => a.use.localeCompare(b.use));
+      break;
+    case "Technique":
+      searchTechniquesArray.sort((a, b) => a.title.localeCompare(b.title));
+      break;
+    default:
+      break;
+    }
+  helpersUI.drawFoundTechniquesTable(searchTechniquesArray)
+}
+
+function handleTableColorOnClick(){
+  const classListString = this.classList.toString();
+  if (classListString.includes("resultsTable")) {
+    handleColorOnClickResultsTable(this);
+  } else if (classListString.includes("list")) {
+    helpersUI.handleColorOnClickPhaseTactic(this);
+  }
+}
+
+function handleColorOnClickResultsTable(this1){
+
+  const isSelected = this1.classList.toString().includes("selected");
+  if (isSelected) {
+    this1.classList.remove("selected");
+    updateSearchTechniquesColor("white", this1);
+    } 
+  else {
+    this1.classList.add("selected");
+    updateSearchTechniquesColor("blue", this1);
+  }
+}
+
+function updateSearchTechniquesColor(color, this_) {
+  searchTechniquesArray.forEach(technique => {
+    if (technique.id === this_.id) {
+      technique.color = color;
+    }
+  });
 }
--- Disarm-Add-In/src/taskpane/taskpane.js
+++ Disarm-Add-In/src/taskpane/taskpane.js
@@ -3,20 +3,13 @@
  * See LICENSE in the project root for license information.
  */
 /* global     document, Office, Word */
-import * as helpers from './../utils/helpers.js';
+
 import * as helpersUI from '../utils/helpers-ui.js';
-import { getTechniques, searchTechniques } from '../services/api-services.js';
-import { changeRedTagColor, changeRedTagColorSaveBtn, getRedTagColor } from '../components/formatting-component.js';
-import { showPhaseOptions, saveButton } from '../components/red-tag-component.js';
+import { changeRedTagColor, changeRedTagColorSaveBtn } from '../components/formatting-component.js';
+import { insertRedTag, searchRedTag, saveButtonInsertTechniques, displaySearchTechniques, 
+  saveButtonSearchTechniques, clearSearchTechniquesArray } from '../components/red-tag-component.js';
+import { insertRedTable } from '../components/table-summaries-component.js';
 
-import { 
-  searchTechniquesPopup, phaseSearchField, tacticSearchField, checkbox, textBox, 
-  listSearchTechniquesPopup, alertSearchBox, table
-} from '../constants/ui-elements.js';
-
-var jsonTechniques = '';
-var reverseJsonTechniques = '';
-var searchTechniquesArray = []
 
 Office.onReady((info) => {
   if (info.host === Office.HostType.Word) {
@@ -36,7 +29,7 @@ Office.onReady((info) => {
     document.getElementById("format-red-tag-color").onclick = () => tryCatch(changeRedTagColor);
 
     //save
-    document.getElementById("save-btn").onclick = () => tryCatch(finishInsertRedTag);
+    document.getElementById("save-btn").onclick = () => tryCatch(saveButtonInsertTechniques);
     document.getElementById("save-btn1").onclick = () => tryCatch(displaySearchTechniques);
     document.getElementById("save-btn2").onclick = () => tryCatch(changeRedTagColorSaveBtn);
     document.getElementById("save-btn3").onclick = () => tryCatch(saveButtonSearchTechniques);
@@ -45,12 +38,6 @@ Office.onReady((info) => {
     document.querySelectorAll('.close').forEach(element => element.onclick = () => tryCatch(closeButton));
 
   }
-});
-
-$(document).on('click','tr',function(e) { handleTableColorOnClick.call(this) }); 
-$(document).on('click', 'th', function(e) {
-  e.stopPropagation(); 
-  sortTechniquesTable.call(this);
 });
 
 
@@ -63,205 +50,14 @@ async function tryCatch(callback) {
 }
 
 async function closeButton(){
-  await Word.run(async (context) => {
-
-    helpersUI.closeAllFields();
-    helpersUI.unblurBackground();
-    searchTechniquesArray = [];
-  })
-}
-
-
-//insert red tag 
-
-async function insertRedTag() {
-  await Word.run(async (context) => {
-    helpersUI.blurBackground();
-    await showPhaseOptions(true);
-    helpersUI.clearInsertRedTagFields();
-  });
-}
-
-async function finishInsertRedTag(){
-  await saveButton();
-}
-
-//search red tag
-
-async function searchRedTag(){
   
-  searchTechniquesPopup.style.display = 'block';
-  tacticSearchField.innerHTML = '';
-  helpersUI.blurBackground()
-  await showPhaseOptions()  
-}
+  helpersUI.closeAllFields();
+  helpersUI.unblurBackground();
+  clearSearchTechniquesArray();
 
-async function displaySearchTechniques(){
-
-  if (textBox.value == "") {
-    alertSearchBox.innerHTML = 'Please fill out this field!';
-  }
-  else {
-    searchTechniquesPopup.style.display = 'none';
-    if (reverseJsonTechniques == "") reverseJsonTechniques = await searchTechniques()
-
-    searchTechniquesArray = helpers.searchTechniquesFromJson(
-      helpers.getSelectedRowText(phaseSearchField), 
-      helpers.getSelectedRowText(tacticSearchField), 
-      checkbox.checked, 
-      textBox.value, 
-      reverseJsonTechniques)
-
-    helpersUI.drawFoundTechniquesTable(searchTechniquesArray)
-
-    listSearchTechniquesPopup.style.display = 'block';
-    textBox.value = '';
-    checkbox.checked = ''
-    alertSearchBox.innerHTML = ''
-  }
 }
 
 
-function sortTechniquesTable(){
 
-  var column = $(this).text().trim();
-  switch (column) {
-    case "Phase":
-      searchTechniquesArray.sort((a, b) => a.phase.localeCompare(b.phase));
-      break;
-    case "Tactic":
-      searchTechniquesArray.sort((a, b) => a.use.localeCompare(b.use));
-      break;
-    case "Technique":
-      searchTechniquesArray.sort((a, b) => a.title.localeCompare(b.title));
-      break;
-    default:
-      break;
-    }
-  helpersUI.drawFoundTechniquesTable(searchTechniquesArray)
-}
 
-function handleTableColorOnClick(){
-  const classListString = this.classList.toString();
-  if (classListString.includes("resultsTable")) {
-    handleColorOnClickResultsTable(this);
-  } else if (classListString.includes("list")) {
-    helpersUI.handleColorOnClickPhaseTactic(this);
-  }
-}
-
-function handleColorOnClickResultsTable(this1){
-
-  const isSelected = this1.classList.toString().includes("selected");
-  if (isSelected) {
-    this1.classList.remove("selected");
-    updateSearchTechniquesColor("white", this1);
-    } 
-  else {
-    this1.classList.add("selected");
-    updateSearchTechniquesColor("blue", this1);
-  }
-}
-
-function updateSearchTechniquesColor(color, this_) {
-  searchTechniquesArray.forEach(technique => {
-    if (technique.id === this_.id) {
-      technique.color = color;
-    }
-  });
-}
-
-async function saveButtonSearchTechniques(){
-  await Word.run(async (context) => {
-
-    const tagText = helpers.createTagTextSearch(table)
-
-    listSearchTechniquesPopup.style.display = 'none';
-    phaseSearchField.innerHTML = '';
-    tacticSearchField.innerHTML = '';
-    helpersUI.unblurBackground();
-
-    //get sentence
-    var sentenceInitialRange = context.document.getSelection().getTextRanges(['\n', '.', '?'], true);
-    context.load(sentenceInitialRange);
-    var sentenceRange =  await context.sync().then(() => { return sentenceInitialRange.items[0]})
-
-    var sentenceWithoutDot = (sentenceRange.text).slice(0, -1); 
-     
-    //insert tag text to end of sentence
-    sentenceRange.insertText(sentenceWithoutDot + ' ' + tagText + '.', Word.InsertLocation.replace);
-
-    //search tag inside range to highlight
-    var rangeToHighlight = sentenceRange.search(tagText);
-    rangeToHighlight.load('items');
-    await context.sync();
-   
-    //change highlight color of the tag
-    rangeToHighlight.items[0].font.set({highlightColor: getRedTagColor()});
-
-    searchTechniquesArray = []
-
-    await context.sync(); 
-  })
-}
-
-//summaries
-
-async function insertRedTable() {
-
-  await Word.run(async (context) => {
-    
-    //get search json only once
-    if (reverseJsonTechniques == '') reverseJsonTechniques = await searchTechniques();
-    
-    const tag_ids = reverseJsonTechniques.ids
-    
-    // get text of the entire document
-    var wholeDocument = context.document.body
-    wholeDocument.load("text")
-    await context.sync();
-    var text = wholeDocument.text
-    
-    //match cases using regular expressions
-    const smallBracketRegex = /\(([^()]*)\)/g;
-    const middleBracketRegex = /\[(.*?)\]/g;
-
-    const foundSmallBrackets = text.matchAll(smallBracketRegex);
-
-    let foundMiddleBrackets = []
-    let table = [["Technique title", "ID", "Text", "Use"]]
-
-    for (const matches of foundSmallBrackets){
-
-      foundMiddleBrackets = matches[0].match(middleBracketRegex);
-      const commasNumber = (matches[0].match(/,/g) || []).length;
-      let selectedText = helpers.extractTextFromBrackets(text, matches.index)
-
-      if (foundMiddleBrackets.length != commasNumber + 1 || foundMiddleBrackets.length == 0) continue;
-
-      for (let j = 0; j < foundMiddleBrackets.length; j++){
-        
-        let tag = foundMiddleBrackets[j].slice(1, -1)
-        let tagObject = tag_ids[tag]
-
-        selectedText = selectedText.replace(/^\s*/, '');
-        
-        // add matched cases into table
-        table.push([tagObject.title, tag, selectedText, tagObject.use]) 
-      }
-    }
-    table = helpers.sortRedSummariesTable(table);
-    drawTable(table)
-  }
-)}
-
-async function drawTable(table){
-  await Word.run(async (context) => {
-
-    const secondParagraph = context.document.body.paragraphs.getLast()
-    const insertedTable = secondParagraph.insertTable(table.length, 4, Word.InsertLocation.after, table);
-    insertedTable.styleBuiltIn = Word.BuiltInStyleName.listTable3_Accent1;
-})
-
-};
 
