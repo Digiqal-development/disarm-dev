37784200a91835bbecb995b7a264d741a3695a0d
ekarahodza1 <56482990+ekarahodza1@users.noreply.github.com>
2024-09-25 15:54:53 +0200

    refactoring and readme

--- Disarm-Add-In/src/constants/global-variables.js
+++ Disarm-Add-In/src/constants/global-variables.js
@@ -1,5 +1,2 @@
-export var jsonTechniques = '';
-export var reverseJsonTechniques = '';
-export var searchTechniquesArray = []
 export const SEARCH_TABLE_SELECT_COLOR = "rgb(85, 172, 227)"
 export const PHASES = ["Plan", "Prepare", "Execute", "Assess"]
