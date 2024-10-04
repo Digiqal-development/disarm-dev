fed70c9f5f01ce8fd36981e7c7a07f42b95eac2f
ekarahodza1 <56482990+ekarahodza1@users.noreply.github.com>
2024-10-05 00:16:18 +0200

    new framework - tags and techniques updated

--- Disarm-Add-In/src/taskpane/taskpane.js
+++ Disarm-Add-In/src/taskpane/taskpane.js
@@ -14,6 +14,8 @@ import { insertRedTable } from '../components/table-summaries-component.js';
 Office.onReady((info) => {
   if (info.host === Office.HostType.Word) {
 
+
+    document.getElementById("test1").onclick = () => tryCatch(test1);
     //red tag
     document.getElementById("insert-red-tag").onclick = () => tryCatch(insertRedTag);
     document.getElementById("search-red-tag").onclick = () => tryCatch(searchRedTag);
@@ -50,6 +52,41 @@ async function closeButton(){
   helpersUI.closeAllFields();
   helpersUI.unblurBackground();
   clearSearchTechniquesArray();
+
+}
+
+async function test1(){
+  Word.run(function (context) {
+    var body = context.document.body;
+
+    // Insert a section break before the new page
+    body.insertBreak(Word.BreakType.sectionNext, Word.InsertLocation.end);
+
+    // Load the new section to modify its properties
+    var sections = context.document.sections;
+    context.load(sections);
+
+    return context.sync().then(function () {
+        // Get the last section (the one we just inserted)
+        var lastSection = sections.items[sections.items.length - 1];
+
+        // Set the orientation of the new section to landscape
+        console.log("ok")
+        console.log(lastSection)
+        console.log(Word.PageOrientation.landscape)
+
+        
+        lastSection.pageOrientation = Word.PageOrientation.landscape;
+
+        console.log("ok")
+
+        return context.sync().then(function () {
+            console.log('New section in landscape orientation added.');
+        });
+    });
+}).catch(function (error) {
+    console.log('Error: ' + error);
+});
 
 }
 
